d001d00c6b3d0a7c7f5105f68e589abd395813bf
José Geovanny Mejía Ramos <pp1234ramos@gmail.com>
2026-09-23 21:01:36 -0500

    Clarity

--- app/cafeteria_en_casa/layout.tsx
+++ app/cafeteria_en_casa/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
 };
 
 const META_PIXEL_ID = "987224667726544";
+const CLARITY_PROJECT_ID = "yn3a4rlr8n";
 
 export default function RootLayout({
   children,
@@ -24,32 +25,48 @@ export default function RootLayout({
       <body>
         {children}
 
-        {/* Meta Pixel */}
+        {/* =========================
+            META PIXEL
+        ========================== */}
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: ` 
-              !function(f,b,e,v,n,t,s) {
-                if(f.fbq)return;
-                n=f.fbq=function(){
-                n.callMethod? 
-                n.callMethod.apply(n,arguments):
-                n.queue.push(arguments)}; 
-                if(!f._fbq)
-                f._fbq=n;
-                n.push=n;
-                n.loaded=!0;
-                n.version='2.0'; 
-                n.queue=[];
-                t=b.createElement(e);
-                t.async=!0; 
-                t.src=v;
-                s=b.getElementsByTagName(e)[0]; 
-                s.parentNode.insertBefore(t,s)}(window, document,'script', 
-                'https://connect.facebook.net/en_US/fbevents.js'); 
-                fbq('init', '${META_PIXEL_ID}'); 
-                fbq('track', 'PageView'); `,
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* =========================
+            MICROSOFT CLARITY
+        ========================== */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){
+                    (c[a].q=c[a].q||[]).push(arguments)
+                  };
+                  t=l.createElement(r);
+                  t.async=1;
+                  t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];
+                  y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+            `,
           }}
         />
       </body>
