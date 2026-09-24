92fe6216bb336e458208c8867bd62e2d19be0986
José Geovanny Mejía Ramos <pp1234ramos@gmail.com>
2026-09-23 19:49:25 -0500

    Update layout.tsx

--- app/cafeteria_en_casa/layout.tsx
+++ app/cafeteria_en_casa/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,6 +12,8 @@ export const metadata: Metadata = {
   },
 };
 
+const META_PIXEL_ID = "987224667726544";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +21,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` 
+              !function(f,b,e,v,n,t,s) {
+                if(f.fbq)return;
+                n=f.fbq=function(){
+                n.callMethod? 
+                n.callMethod.apply(n,arguments):
+                n.queue.push(arguments)}; 
+                if(!f._fbq)
+                f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0'; 
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0; 
+                t.src=v;
+                s=b.getElementsByTagName(e)[0]; 
+                s.parentNode.insertBefore(t,s)}(window, document,'script', 
+                'https://connect.facebook.net/en_US/fbevents.js'); 
+                fbq('init', '${META_PIXEL_ID}'); 
+                fbq('track', 'PageView'); `,
+          }}
+        />
+      </body>
     </html>
   );
 }
